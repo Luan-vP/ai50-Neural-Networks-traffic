3ffb6fe6da953c8bf4fd4589391932d55370af7e
Luan-vP <luanvanpletsen@gmail.com>
2021-05-20 22:44:40 +0100

    Implement model()

--- traffic.py
+++ traffic.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 import sys
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 from sklearn.model_selection import train_test_split
 
@@ -84,7 +86,30 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+
+    # Input layer of shape (IMG_WIDTH, IMG_HEIGHT, 3)
+    # Output layer is NUM_CATEGORIES
+    # Convolution and pooling
+    # Hidden Layers
+
+    model = keras.Sequential(
+        [
+            layers.Conv2D(32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            layers.Dense(128, activation="relu"),
+            layers.Dropout(0.5),
+            layers.Dense(NUM_CATEGORIES, activation="softmax")
+        ]
+    )
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
 
 
 if __name__ == "__main__":
